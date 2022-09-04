29971e052d7d104a1ba314ae4bb2ef75190b4ac3
mashroorzisan <mashroorzisan@gmail.com>
2022-09-05 00:37:56 +0600

    made the website

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,28 @@
-console.log('weather')
+const cityName = city => {
+    const key = 'ed18233e9531aa1fb631f8c133788b50';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayData(data))
+}
+const displayData = logs => {
+    console.log(logs);
+    const citySection = document.getElementById('citysection');
+    citySection.textContent = '';
+    const cityDiv = document.createElement('div');
+    cityDiv.innerHTML = `
+    <h1 class= 'font-bold text-white text-5xl '>${logs.name}</h1>
+    <p class ='font-semibold text-white text-2xl mt-10'>Today's temperature at dhaka is</p>
+    <h1 class = 'font bold text-white text-4xl'>${logs.main.temp}°C</h1>
+    `;
+    citySection.appendChild(cityDiv)
+
+}
+
+
+document.getElementById('src-btn').addEventListener('click', function () {
+    // console.log('hello')
+    const inputField = document.getElementById('src-value');
+    cityName(inputField.value);
+})
+
